8b5a9bffbdc600aa27fccc64d324ee6e9820c88e
elmahdi <elmhadrielmahdi@gmail.com>
2026-03-10 06:51:34 +0000

    g

--- test_app.py
+++ test_app.py
@@ -1,23 +1,4 @@
-import pytest
-from app import add, subtract, multiply, divide
+from app import add
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(-1, -1) == -2
-
-def test_subtract():
-    assert subtract(5, 3) == 2
-    assert subtract(3, 5) == -2
-    assert subtract(-1, -1) == 0
-
-def test_multiply():
-    assert multiply(2, 3) == 6
-    assert multiply(-2, 3) == -6
-    assert multiply(0, 5) == 0
-
-def test_divide():
-    assert divide(6, 3) == 2
-    assert divide(5, 2) == 2.5
-    with pytest.raises(ValueError):
-        divide(5, 0)
+    assert add(2,3) == 5
